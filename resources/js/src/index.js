76c706e6d828535112314506a15ceb3dd83dc0bf
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2021-10-20 16:03:32 -0300

    init

---
+++ resources/js/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+export const Teste = ()=>{
+    return(
+        <h1>React Work!</h1>
+    )
+}
+ReactDOM.render(<Teste/>,document.getElementById('root'))
